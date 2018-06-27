779fa8ff7fe67324006b0a674ed5bc0044a80020
Ryoalrui <Ryoalrui@126.com>
2018-06-27 12:39:14 +0800

    test upload

--- test/test.php
+++ test/test.php
@@ -1,6 +1,7 @@
-<?PHP
-function trans_bytes(string $filesize){
-$level=['B','KB','MB','GB','TB'];
-$key=floor(log($filesize,1024));
-$res=round($filesize/pow(1024,$key),2).$level[$key];
+<?php
+function trans_bytes(string $filesize)
+{
+    $level=['B','KB','MB','GB','TB'];
+    $key=floor(log($filesize, 1024));
+    $res=round($filesize/pow(1024, $key), 2).$level[$key];
 }
