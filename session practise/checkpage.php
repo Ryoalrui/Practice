b1115062b095cf317e6ede98f25bacbb45a4d168
Ryoalrui <Ryoalrui@126.com>
2018-06-28 17:22:01 +0800

    update file of logout button

--- session practise/checkpage.php
+++ session practise/checkpage.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
-
+date_default_timezone_set('Asia/shanghai');
 error_reporting(E_ALL &~ E_NOTICE);
+
+$hour = date('H');
+
 // if ($_COOKIE['username'] === 'zhangsan') {
 //     echo '欢迎你，先生！';
 // } else {
@@ -9,7 +12,33 @@ error_reporting(E_ALL &~ E_NOTICE);
 // }
 
 if ($_SESSION['zhangsan'] === '12345678') {
-    echo '晚上好，先生！';
+    if ($hour>=6 && $hour<10) {
+        echo '早上好，先生！';
+    } elseif ($hour>=10 && $hour<=14) {
+        echo '中午好，先生！';
+    } elseif ($hour>14 && $hour<=18) {
+        echo '下午好，先生！';
+    } else {
+        echo '晚上好，先生！';
+    }
 } else {
-    echo '无权限查看';
+    $res = '无权限查看';
+    echo $res;
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>欢迎界面</title>
+</head>
+<body>
+  <?php if ($res!='无权限查看') {
+    ?>
+    <button><a href="http://localhost/Demo/01/Session/%E5%8D%83%E5%B3%B0%E5%AE%9E%E4%BE%8B/logout.php" target="_self" style="text-decoration:none">退出</a></button>
+  <?php
+} ?>
+</body>
+</html>
