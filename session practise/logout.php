e2e6dbf0365840ecbe8ec33ade149235f1e99a9d
Ryoalrui <Ryoalrui@126.com>
2018-06-28 17:24:17 +0800

    update hint of logout

--- session practise/logout.php
+++ session practise/logout.php
@@ -4,3 +4,16 @@ session_start();
 
 $_SESSION['zhangsan']=null;
 session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>退出界面</title>
+</head>
+<body>
+  <h1>你已退出</h1>
+</body>
+</html>
