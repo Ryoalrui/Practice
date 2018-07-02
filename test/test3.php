7bec1de39b27d535079185f94f4db0df03875674
Ryoalrui <39912950+Ryoalrui@users.noreply.github.com>
2018-07-02 23:13:39 +0800

    json with php practise

--- test/test3.php
+++ test/test3.php
@@ -1,1 +1,5 @@
-backup file
+$arr = ['apple','banana','orange'];
+$narr1 = json_encode($arr);//进行json编码，返回字符串，包含了 value 值 JSON 形式的表示。
+
+$narr2 = json_decode($narr1);//进行json解码还原
+echo $narr1.'<br/>'.$narr2;
