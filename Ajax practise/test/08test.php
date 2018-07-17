e4d1755c7d6420edccfd04619995c47f1d68b8d2
Ryoalrui <Ryoalrui@126.com>
2018-07-17 11:58:26 +0800

    test file four

---
+++ Ajax practise/test/08test.php
@@ -0,0 +1,10 @@
+<?php
+header("content-type:text/html;charser=UTF-8");
+$val = $_GET['pos'];//get形式获取值
+// $val = $_POST['pos'];//post形式获取值
+
+parse_str($val, $get);
+$level = ['B','KB','MB','GB','TB'];
+$key = floor(log($get['num'], 1024));
+$res = round($get['num']/(pow(1024, $key)), 2).$level[$key];
+echo json_encode(['transform'=>$res]);//单个数据就不要用json了
