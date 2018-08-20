999e591df43856b088439fa4dbe64a0e9268e789
Ryoalrui <Ryoalrui@126.com>
2018-08-20 23:47:12 +0800

    index file|update file

--- test/HW TEST/02/index.php
+++ test/HW TEST/02/index.php
@@ -1,10 +1,10 @@
 <?php
 header('content-type:text/html;charset=UTF-8');
+//连接数据库，设置字符集，查询数据库内的相关信息，将其显示在前端页面
 $mysqli = new mysqli('localhost', 'root', '1010LING', 'hw');
 $mysqli->query('set names utf8');
-$data = $mysqli->query('SELECT name,addr FROM pic_upload_system');
-$res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
-// print_r($res);
+$data = $mysqli->query('SELECT info,addr FROM pic_upload_system');
+$res = array_reverse($data->fetch_all(MYSQLI_ASSOC));//数据信息进行倒序摆放
 ?>
 
 <!DOCTYPE html>
@@ -36,6 +36,7 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
     </style>
 </head>
 
+<?php if ($_GET['search']==''): ?>
 <body>
     <!--导航栏-->
     <div class="navbar navbar-inverse navbar-fixed-top">
@@ -47,7 +48,7 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand hidden-sm" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'navbar-首页'])">酷图</a>
+                <a class="navbar-brand hidden-sm" href="" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'navbar-首页'])">酷图</a>
             </div>
             <div class="navbar-collapse collapse" role="navigation">
                 <ul class="nav navbar-nav">
@@ -68,9 +69,9 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
             <h1>文件上传下载</h1>
             <h2>实现文件的上传和下载功能</h2>
             <p class="masthead-button-links">
-                <form class="form-inline" action="index.php" method="get">
+                <form class="form-inline" action="search.php" method="get">
                     <div class="form-group">
-                        <input type="text" class="form-control" id="exampleInputName2" placeholder="搜索相关图片" name="key" value="">
+                        <input type="text" class="form-control" id="exampleInputName2" placeholder="输入关键字搜索相关图片" name="key">
                         <button class="btn btn-default" type="submit">搜索</button>
                         <button type="button" class="btn btn-primary btn-default" data-toggle="modal" data-target="#myModal">  上传  </button>
                     </div>
@@ -89,9 +90,9 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
                         <h4 class="modal-title" id="myModalLabel">上传图片</h4>
                     </div>
                     <div class="modal-body">
-                        <p>选择图片：</p><input type="file" id="image" name="upload">
+                        <p>选择图片：</p><input type="file" name="upload">
                         <br/>
-                        <p>图片描述：</p><textarea class="form-control" cols="75" name="info"></textarea>
+                        <p>图片描述：</p><textarea class="form-control" cols="75" name="info" id="info"></textarea>
                         <br/><br/>
                         <p>
                           是否添加水印：
@@ -111,7 +112,7 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
                         </p>
                     </div>
                     <div class="modal-footer">
-                        <button type="submit" class="btn btn-default" id="submit">上传</button>
+                        <button type="submit" class="btn btn-default" name="submit" id="submit" onclick="show(this)">上传</button>
                         <button type="reset" class="btn btn-primary">重置</button>
                     </div>
                 </div>
@@ -131,7 +132,7 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
                 <div class="image">
                     <img class="img-responsive" src="<?php echo strstr($single['addr'], 'u');?>">
                     <div class="caption">
-                      <p><?php echo $single['name']; ?></p>
+                      <p><?php echo $single['info']; ?></p>
                     </div>
                 </div>
             </div>
@@ -162,7 +163,7 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
     <footer class="footer  container">
         <div class="row footer-bottom">
             <ul class="list-inline text-center">
-                <h4>best-picture.com | 酷图</h4>
+                <h4>best pic.com | 酷图</h4>
             </ul>
         </div>
     </footer>
@@ -173,11 +174,156 @@ $res = array_reverse($data->fetch_all(MYSQLI_ASSOC));
     <script src="style/js/bootstrap.min.js"></script>
     <script type="text/JavaScript">
         function show(){
-          if(document.getElementById("image").value == ''){
-            alert('请选择图片');
+          if(document.getElementById("info").value == ''){
+            alert('请输入图片描述');
           }
         }
     </script>
 </body>
+
+<?php else: ?>
+
+<body>
+    <!--导航栏-->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand hidden-sm" href="" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'navbar-首页'])">酷图</a>
+            </div>
+            <div class="navbar-collapse collapse" role="navigation">
+                <ul class="nav navbar-nav">
+                    <li class="hidden-sm hidden-md">
+                        <a href="" target="_blank"></a>
+                    </li>
+                    <li>
+                        <a href="" target="_blank"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--导航栏结束-->
+    <!--巨幕-->
+    <div class="jumbotron masthead">
+        <div class="container">
+            <h1>文件上传下载</h1>
+            <h2>实现文件的上传和下载功能</h2>
+            <p class="masthead-button-links">
+                <form class="form-inline" action="search.php" method="get">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleInputName2" placeholder="输入关键字搜索相关图片" name="key">
+                        <button class="btn btn-default" type="submit">搜索</button>
+                        <button type="button" class="btn btn-primary btn-default" data-toggle="modal" data-target="#myModal">  上传  </button>
+                    </div>
+                </form>
+            </p>
+        </div>
+    </div>
+    <!--巨幕结束-->
+    <!-- 模态框 -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <form class="form-inline" action="handle.php" method="post" enctype="multipart/form-data">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">上传图片</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>选择图片：</p><input type="file" name="upload">
+                        <br/>
+                        <p>图片描述：</p><textarea class="form-control" cols="75" name="info" id="info"></textarea>
+                        <br/><br/>
+                        <p>
+                          是否添加水印：
+                          <select name="mark">
+                            <option value="1">添加</option>
+                            <option value="0">不添加</option>
+                          </select>
+                        </p>
+                        <br/>
+                        <p>
+                          图片宽度比例：
+                            <select name="scale">
+                              <option value="800*600">800*600</option>
+                              <option value="600*450">600*450</option>
+                              <option value="400*300">400*300</option>
+                            </select>
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-default" name="submit" id="submit" onclick="show(this)">上传</button>
+                        <button type="reset" class="btn btn-primary">重置</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--模态框结束-->
+
+    <div class="container projects">
+        <div class="projects-header page-header">
+            <h2>上传图片展示</h2>
+            <p>将上传的图片展示在页面中</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-3 col-lg-4 ">
+                <div class="image">
+                    <img class="img-responsive" src="<?php echo strstr($_GET['addr'], 'u');?>">
+                    <div class="caption">
+                      <p><?php echo $_GET['search']; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--分页-->
+        <nav aria-label="Page navigation" style="text-align:center">
+            <ul class="pagination pagination-lg">
+                <li>
+                    <a href="" aria-label="Previous">
+                        <span aria-hidden="true">首页</span>
+                    </a>
+                </li>
+                <li><a href="">1</a></li>
+                <li><a href="">2</a></li>
+                <li><a href="">3</a></li>
+                <li><a href="">4</a></li>
+                <li><a href="">5</a></li>
+                <li>
+                    <a href="" aria-label="Next">
+                        <span aria-hidden="true">尾页</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <footer class="footer  container">
+        <div class="row footer-bottom">
+            <ul class="list-inline text-center">
+                <h4>best pic.com | 酷图</h4>
+            </ul>
+        </div>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="style/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="style/js/bootstrap.min.js"></script>
+    <script type="text/JavaScript">
+        function show(){
+          if(document.getElementById("info").value == ''){
+            alert('请输入图片描述');
+          }
+        }
+    </script>
+</body>
+<?php endif; ?>
 
 </html>
