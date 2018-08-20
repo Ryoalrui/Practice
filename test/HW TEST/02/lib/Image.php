a9b5ac21db8c4015569b32e40b8fe147d9259e64
Ryoalrui <Ryoalrui@126.com>
2018-08-20 23:52:09 +0800

    class of resize and mark|update file

--- test/HW TEST/02/lib/Image.php
+++ test/HW TEST/02/lib/Image.php
@@ -4,13 +4,13 @@ header('content-type:text/html;charset=UTF-8');
 
 class Image
 {
-    protected $_width;
-    protected $_height;
-    protected $_im;
-    protected $_type;
-    protected $_mine;
-    protected $_real_path;
-    protected $font = 'E:/wamp64/www/Demo/HW/UploadDemo/style/fonts/1_0.ttf';
+    protected $_width;//图片宽度
+    protected $_height;//图片高度
+    protected $_im;//图像标识符
+    protected $_type;//图像后缀类型
+    protected $_mine;//图像类型
+    protected $font = 'E:/wamp64/www/Demo/HW/UploadDemo/style/fonts/1_0.ttf';//预设字体位置
+
 
     public function __construct($file)
     {
@@ -21,13 +21,12 @@ class Image
         $this->_im=$imageInfo['im'];
         $this->_type=$imageInfo['type'];
         $this->_mime=$imageInfo['mime'];
-        $this->_real_path=$imageInfo['real_path'];
     }
 
     /**
-     * 根据文件来创建对象
+     * 获取上传图片参数
      * @method createImageByFile
-     * @param  [file] $file [文件地址]
+     * @param  [file] $file [图片地址]
      * @return array
      * @throws /Exception
      */
@@ -39,7 +38,6 @@ class Image
         }
         //获取图像信息
         $imageInfo=getimagesize($file);
-        $realPath=realpath($file);
         if (!$imageInfo) {
             throw new Exception('file is not an image file');
         }
@@ -62,7 +60,6 @@ class Image
           'type'=>$imageInfo[2],
           'mime'=>$imageInfo['mime'],
           'im'=>$im,
-          'real_path'=>$realPath
         );
     }
 
@@ -77,15 +74,37 @@ class Image
     public function Resize($width, $height)
     {
         if (!is_numeric($width) || !is_numeric($height)) {
-            throw new Exception('image width or height must be number');
+            throw new \Exception('image width or height must be number');
         }
         //根据传参的宽高获取最终图像的宽高(实际传进来的参数不见得能成功缩放图像，可能存在长宽比例失衡)
         $srcW=$this->_width;
         $srcH=$this->_height;
 
-        $desW=$width;//缩略图高度
-        $desH=$height;//缩略图宽度
+        if ($width<=0 || $height<=0) {//参数为0，则套用原来的宽高
+             $desW=$srcW;//缩略图高度
+             $desH=$srcH;//缩略图宽度
+        } else {
+            $srcP=$srcW/$srcH;//旧宽高比例
+            $desP=$width/$height;//新宽高比例
 
+           if ($width>$srcW) {
+               if ($height>$srcH) {
+                   $desW=$srcW;
+                   $desH=$srcH;
+               } else {
+                   $desH=$height;
+                   $desW=round($desH*$srcP);
+               }
+           } else {
+               if ($desP>$srcP) {
+                   $desW=$width;
+                   $desH=round($desW/$srcP);
+               } else {
+                   $desH=$height;
+                   $desW=round($desH*$srcP);
+               }
+           }
+        }
         //PHP版本小于5.5
         if (version_compare(PHP_VERSION, '5.5.0', '<')) {
             $desIm=imagecreatetruecolor($desW, $desH);
@@ -105,10 +124,15 @@ class Image
         return $this;
     }
 
+    /**
+     * 添加文字水印
+     * @method stringMark
+     * @return Image
+     */
     public function stringMark()
     {
         $s_color = imagecolorallocate($this->_im, 255, 255, 255);
-        imagettftext($this->_im, 28, 0, $this->_width/2.5, $this->_height/1.1, $s_color, $this->font, 'power By Ryo');
+        imagettftext($this->_im, 18, 0, $this->_width/2.5, $this->_height/1.1, $s_color, $this->font, 'power By Ryo');
         return $this;
     }
 
@@ -116,10 +140,9 @@ class Image
      * 保存图像文件
      * @method save
      * @param  [file] $file
-     * @param  [int] $quality
      * @throws /Exception
      */
-    public function Save($file, $quality=60)
+    public function Save($file)
     {
         //获取保存目的文件的扩展名
         $ext=strtolower(pathinfo($file, PATHINFO_EXTENSION));
@@ -130,19 +153,8 @@ class Image
             imagegif($this->_im, $file);
             return true;
         } elseif ($ext==='jpeg' || $ext==='jpg') {
-            if ($quality>0) {//如果质量参数大于0
-                if ($quality<1) {//但小于1，则按1填入
-                    $quality=1;
-                }
-                if ($quality>100) {//且大于100，则按100填入
-                    $quality=100;
-                }
-                imagejpeg($this->_im, $file, $quality);
-                return ture;
-            } else {//如果没有填入质量参数，则默认按100%填写
-                imagejpeg($this->_im, $file);
-                return true;
-            }
+            imagejpeg($this->_im, $file, 70);
+            return ture;
         } else {
             imagepng($this->_im, $file);
             return true;
