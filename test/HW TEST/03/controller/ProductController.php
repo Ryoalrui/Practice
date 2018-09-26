c8f4272094db0015fd6a0ecd51638c1f6a0b5b51
Ryoalrui <Ryoalrui@126.com>
2018-09-26 10:33:36 +0800

    product

--- test/HW TEST/03/controller/ProductController.php
+++ test/HW TEST/03/controller/ProductController.php
@@ -1,18 +1,39 @@
 <?php
 header('content-type:text/html;charset=UTF-8');
 require_once 'model/Database.php';
+require_once 'lib/Pagination.php';
 
 class ProductController
 {
     public function course_list()
     {
-        require_once 'view/list.html';
-    }
-    public function insert()
-    {
         $db = new Database();
-        $db->insert($_POST);
-        // print_r($_POST);
-        // var_dump($db);
+        switch ($_GET['key']) {
+          case 'PHP':
+          $res = $db->search('PHP');
+          break;
+          case 'Java':
+          $res = $db->search('Java');
+          break;
+          case 'Html/css':
+          $res = $db->search('Html/css');
+          break;
+          case 'Python':
+          $res = $db->search('Python');
+          break;
+          case 'Android':
+          $res = $db->search('Android');
+          break;
+          case 'Ios':
+          $res = $db->search('Ios');
+          break;
+          default:
+          $number = $db->query('SELECT COUNT(*) AS nums FROM course');
+          $page = new Page(6, $number[0]['nums']);
+          $res = $db->query('SELECT * FROM course LIMIT '.$page->limit());
+          $page->allUrl();
+          break;
+        }
+        require_once 'view/list.html';
     }
 }
