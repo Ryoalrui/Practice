53802c2329f8634906a9f4f81587c33ed4ed62fc
Ryoalrui <Ryoalrui@126.com>
2018-10-12 23:48:26 +0800

    ShoppingCart

--- test/HW TEST/03/controller/ShoppingController.php
+++ test/HW TEST/03/controller/ShoppingController.php
@@ -1,8 +1,23 @@
 <?php
+header('content-type:text/html;charset=UTF-8');
+require_once 'model/Database.php';
+
 class ShoppingController
 {
     public function shopping_list()
     {
+        $db = new Database();
+        $res = $db->query('SELECT * FROM shopping_cart');
         require_once 'view/shopping_list.html';
+    }
+    public function add()
+    {
+        $db = new Database();
+        $db->add($_POST);
+    }
+    public function drop()
+    {
+        $db = new Database();
+        var_dump($db->del($_POST));
     }
 }
